1cbb1c537cbb40b683f5ad73913e201596d3bff7
Mykhailo Kaskun <tmikent@gmail.com>
2018-11-25 22:20:32 +0200

    Add cors & optimize/change create and delete functions

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp();
+const cors = require('cors')({ origin: true });
 
 function bodyToSnippet({ author, title, data, type }) {
     return { 
@@ -15,98 +16,106 @@ function getSnippetSnapshot(id) {
 }
 
 export const ping = functions.https.onRequest((req, res) => {
-    res.send('Successfully pinged!\n\n');
+    return cors(req, res, () => {
+        res.send('Successfully pinged!\n\n');
+    });
 });
 
 // POST method to create snippet
 export const createSnippet = functions.https.onRequest((req, res) => {
-    if (req.method !== 'POST') {
-        return res.status(405).header('Allow', 'POST').json({
-            message: 'Send a POST request.'
-        });
-    }
-    
-    const snippet = bodyToSnippet(req.body);
-    if (!snippet.data) {
-        return res.status(400).json({
-            message: 'Property "data" is required.'
-        });
-    }
+    return cors(req, res, async () => {
+        if (req.method !== 'POST') {
+            return res.status(405).header('Allow', 'POST').json({
+                message: 'Send a POST request.'
+            });
+        }
 
-    const privateUid = admin.database().ref('snippets').push(snippet).key;
-    const publicUid = admin.database().ref('readonlyProxy').push({
-        'snippet': privateUid
-    }).key;
+        const snippet = bodyToSnippet(req.body);
+        if (!snippet.data) {
+            return res.status(400).json({
+                message: 'Property "data" is required.'
+            });
+        }
 
-    return res.json({ privateUid, publicUid });
+        const privateUid = admin.database().ref('snippets').push(snippet).key;
+        const publicUid = admin.database().ref('readonlyProxy').push({
+            'snippet': privateUid
+        }).key;
+        await admin.database().ref(`snippets/${privateUid}`).update({ publicUid });
+
+        return res.json({ privateUid, publicUid });
+    });
 });
 
 // DELETE method to delete snippet
-export const deleteSnippet = functions.https.onRequest(async (req, res) => {
-    if (req.method !== 'DELETE') {
-        return res.status(405).header('Allow', 'DELETE').json({
-            message: 'Send a DELETE request.'
-        });
-    }
+export const deleteSnippet = functions.https.onRequest((req, res) => {
+    return cors(req, res, async () => {
+        if (req.method !== 'DELETE') {
+            return res.status(405).header('Allow', 'DELETE').json({
+                message: 'Send a DELETE request.'
+            });
+        }
 
-    const id = req.query.id;
-    if (!id) {
-        return res.status(400).json({
-            message: 'Search parameter "id" is required.'
-        });
-    }
+        const id = req.query.id;
+        if (!id) {
+            return res.status(400).json({
+                message: 'Search parameter "id" is required.'
+            });
+        }
 
-    const snapshot = await getSnippetSnapshot(id);
-    const proxySnippet = await admin.database().ref('readonlyProxy').
-        orderByChild('snippet').equalTo(id).once('value');
-    if (snapshot.exists()) {
-        const promises = [admin.database().ref(snapshot.ref).remove()];
-        proxySnippet.forEach(child => {
-            promises.push(child.ref.remove());
-            return true;
-        });
-        await Promise.all(promises);
-        return res.json({
-            message: `Snippet ${id} is successfully deleted.`
-        });
-    } else {
-        return res.status(404).json({
-            message: `Snippet ${id} is not found.`
-        });
-    }
+        const snapshot = await getSnippetSnapshot(id);
+        const proxySnippet = await admin.database().ref('readonlyProxy').
+            orderByChild('snippet').equalTo(id).once('value');
+        if (snapshot.exists()) {
+            await Promise.all([
+                admin.database().ref(snapshot.ref).remove(),
+                admin.database().ref(`readonlyProxy/${snapshot.val().publicUid}`).remove()
+            ]);
+            return res.json({
+                message: `Snippet ${id} is successfully deleted.`
+            });
+        } else {
+            return res.status(404).json({
+                message: `Snippet ${id} is not found.`
+            });
+        }
+    });
 });
 
 // GET method to get snippet
-export const getSnippet = functions.https.onRequest(async (req, res) => {
-    if (req.method !== 'GET') {
-        return res.status(405).header('Allow', 'GET').json({
-            message: 'Send a GET request.'
-        });
-    }
-
-    const id = req.query.id;
-    if (!id) {
-        return res.status(400).json({
-            message: 'Search parameter "id" is required.'
-        });
-    }
-
-    const snapshot = await getSnippetSnapshot(id);
-    if (snapshot.exists()) {
-        return res.json({
-            snippet: snapshot.val(),
-            readonly: false
-        });
-    } else {
-        const proxySnapshot = await 
-            admin.database().ref(`readonlyProxy/${id}`).once('value');
-        return proxySnapshot.exists()
-            ? res.json({
-                snippet: await getSnippetSnapshot(proxySnapshot.val().snippet),
-                readonly: true
-            })
-            : res.status(404).json({
-                message: `Snippet ${id} is not found.`
+export const getSnippet = functions.https.onRequest((req, res) => {
+    return cors(req, res, async () => {
+        if (req.method !== 'GET') {
+            return res.status(405).header('Allow', 'GET').json({
+                message: 'Send a GET request.'
             });
-    }
+        }
+
+        const id = req.query.id;
+        if (!id) {
+            return res.status(400).json({
+                message: 'Search parameter "id" is required.'
+            });
+        }
+
+        const snapshot = await getSnippetSnapshot(id);
+        if (snapshot.exists()) {
+            return res.json({
+                snippet: snapshot.val(),
+                readonly: false
+            });
+        } else {
+            const proxySnapshot = await
+                admin.database().ref(`readonlyProxy/${id}`).once('value');
+            return proxySnapshot.exists()
+                ? res.json({
+                    snippet: await getSnippetSnapshot(proxySnapshot.val().snippet),
+                    readonly: true
+                })
+                : res.status(404).json({
+                    message: `Snippet ${id} is not found.`
+                });
+        }
+    });
 });
+
