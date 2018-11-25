0deb4d992e5993b10c859523fd73d06443ef188c
Posivnych <moroler22@gmail.com>
2018-11-25 15:25:17 +0200

    Added code container.

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,217 @@
-import React from "react";
-import "./App.css";
-import { Button } from 'react-bootstrap';
+import React, { Component } from "react";
+import AceEditor from "react-ace";
+import "brace/mode/jsx";
+import "brace/ext/language_tools";
+import "brace/ext/searchbox";
 
-class App extends React.Component {
+const languages = [
+  "javascript",
+  "java",
+  "python",
+  "xml",
+  "ruby",
+  "sass",
+  "markdown",
+  "mysql",
+  "json",
+  "html",
+  "csharp",
+  "typescript",
+  "css"
+];
+
+const themes = [
+  "monokai",
+  "github",
+  "tomorrow",
+  "kuroir",
+  "twilight",
+  "xcode",
+  "textmate",
+  "solarized_dark",
+  "solarized_light",
+  "terminal"
+];
+
+languages.forEach(lang => {
+  require(`brace/mode/${lang}`);
+  require(`brace/snippets/${lang}`);
+});
+
+themes.forEach(theme => {
+  require(`brace/theme/${theme}`);
+});
+
+const defaultValue = `Enter text here...`;
+
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: null,
-      isEnabled: false};
+      value: defaultValue,
+      theme: "monokai",
+      mode: "markdown",
+      enableBasicAutocompletion: true,
+      enableLiveAutocompletion: false,
+      fontSize: 14,
+      showGutter: true,
+      showPrintMargin: false,
+      highlightActiveLine: true,
+      enableSnippets: false,
+      showLineNumbers: true
+    };
+
+    this.setTheme = this.setTheme.bind(this);
+    this.setMode = this.setMode.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.setFontSize = this.setFontSize.bind(this);
+    this.setBoolean = this.setBoolean.bind(this);
   }
 
-  handleChange() {
-    this.setState({isEnabled: !this.state.isEnabled})
+  onChange(newValue) {
+    this.setState({
+      value: newValue
+    });
+  }
+
+  setTheme(e) {
+    this.setState({
+      theme: e.target.value
+    });
+  }
+
+  setMode(e) {
+    this.setState({
+      mode: e.target.value
+    });
+  }
+
+  setBoolean(name, value) {
+    this.setState({
+      [name]: value
+    });
+  }
+
+  setFontSize(e) {
+    this.setState({
+      fontSize: parseInt(e.target.value, 10)
+    });
   }
 
   render() {
     return (
-      <div>
-        <textarea 
-        rows={15}
-        readOnly={this.state.isEnabled ? false : true}
-        >
-          {"html with multiple code snippets"}
-        </textarea>
-        <Button onClick={() => this.handleChange()}>
-          </Button>
+      <div className="columns">
+        <div className="column">
+          <div className="field">
+            <label>Mode:</label>
+            <p className="control">
+              <span className="select">
+                <select
+                  name="mode"
+                  onChange={this.setMode}
+                  value={this.state.mode}
+                >
+                  {languages.map(lang => (
+                    <option key={lang} value={lang}>
+                      {lang}
+                    </option>
+                  ))}
+                </select>
+              </span>
+            </p>
+          </div>
+
+          <div className="field">
+            <label>Theme:</label>
+            <p className="control">
+              <span className="select">
+                <select
+                  name="Theme"
+                  onChange={this.setTheme}
+                  value={this.state.theme}
+                >
+                  {themes.map(lang => (
+                    <option key={lang} value={lang}>
+                      {lang}
+                    </option>
+                  ))}
+                </select>
+              </span>
+            </p>
+          </div>
+
+          <div className="field">
+            <label>Font Size:</label>
+            <p className="control">
+              <span className="select">
+                <select
+                  name="Font Size"
+                  onChange={this.setFontSize}
+                  value={this.state.fontSize}
+                >
+                  {[14, 16, 18, 20, 24, 28, 32, 40].map(lang => (
+                    <option key={lang} value={lang}>
+                      {lang}
+                    </option>
+                  ))}
+                </select>
+              </span>
+            </p>
+          </div>
+
+          <div className="field">
+            <p className="control">
+              <label className="checkbox">
+                <input
+                  type="checkbox"
+                  checked={this.state.enableLiveAutocompletion}
+                  onChange={e =>
+                    this.setBoolean(
+                      "enableLiveAutocompletion",
+                      e.target.checked
+                    )
+                  }
+                />
+                Enable Live Autocomplete
+              </label>
+            </p>
+          </div>
+
+          <div className="field">
+            <p className="control">
+              <label className="checkbox">
+                <input
+                  type="checkbox"
+                  checked={this.state.enableSnippets}
+                  onChange={e =>
+                    this.setBoolean("enableSnippets", e.target.checked)
+                  }
+                />
+                Enable Snippets
+              </label>
+            </p>
+          </div>
+        </div>
+
+        <div className="examples column">
+          <AceEditor
+            mode={this.state.mode}
+            theme={this.state.theme}
+            onChange={this.onChange}
+            value={this.state.value}
+            fontSize={this.state.fontSize}
+            showPrintMargin={this.state.showPrintMargin}
+            showGutter={this.state.showGutter}
+            highlightActiveLine={this.state.highlightActiveLine}
+            setOptions={{
+              enableBasicAutocompletion: this.state.enableBasicAutocompletion,
+              enableLiveAutocompletion: this.state.enableLiveAutocompletion,
+              enableSnippets: this.state.enableSnippets,
+              showLineNumbers: this.state.showLineNumbers,
+              tabSize: 2
+            }}
+          />
+        </div>
       </div>
     );
   }
