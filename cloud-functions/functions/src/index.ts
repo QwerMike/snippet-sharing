188ea57a55f8a705c06ab024b6ca3fc300727ff9
Mykhailo Kaskun <tmikent@gmail.com>
2018-11-25 19:14:23 +0200

    Add function deleteSnippet

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -16,7 +16,7 @@ export const ping = functions.https.onRequest((req, res) => {
 
 // POST method to create snippet
 export const createSnippet = functions.https.onRequest((req, res) => {
-    if (req.method != 'POST') {
+    if (req.method !== 'POST') {
         return res.status(405).header('Allow', 'POST').json({
             message: 'Send a POST request.'
         });
@@ -35,4 +35,39 @@ export const createSnippet = functions.https.onRequest((req, res) => {
     }).key;
 
     return res.json({ privateUid, publicUid });
+});
+
+// DELETE method to delete snippet
+export const deleteSnippet = functions.https.onRequest(async (req, res) => {
+    if (req.method !== 'DELETE') {
+        return res.status(405).header('Allow', 'DELETE').json({
+            message: 'Send a DELETE request.'
+        });
+    }
+
+    const id = req.query.id;
+    if (!id) {
+        return res.status(400).json({
+            message: 'Search parameter "id" is required.'
+        });
+    }
+
+    const snapshot = await admin.database().ref(`snippets/${id}`).once('value');
+    const proxySnippet = await admin.database().ref('readonlyProxy').
+        orderByChild('snippet').equalTo(id).once('value');
+    if (snapshot.exists()) {
+        const promises = [admin.database().ref(snapshot.ref).remove()];
+        proxySnippet.forEach(child => {
+            promises.push(child.ref.remove());
+            return true;
+        });
+        await Promise.all(promises);
+        return res.json({
+            message: `Snippet ${id} is successfully deleted.`
+        });
+    } else {
+        return res.status(404).json({
+            message: `Snippet ${id} is not found.`
+        });
+    }
 });
