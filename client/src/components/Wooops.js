d64617ca778121a17c264be5d41c7ef7ac60ffb2
Posivnych <moroler22@gmail.com>
2018-11-26 01:19:05 +0200

    Fix image path

--- client/src/components/Wooops.js
+++ client/src/components/Wooops.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { Image, Button, ButtonToolbar } from "react-bootstrap";
+import { Image, Button } from "react-bootstrap";
 
 class Wooops extends Component {
     render() {
         return (
             <div align='center'>
                 <h1>Wooops...</h1>
-                <Image className='image' src='./snippets-wooops.png' alt='snippet share wooops' width='30%' height='30%' circle/>
+                <Image className='image' src='/snippets-wooops.png' alt='snippet share wooops' width='30%' height='30%' circle/>
                 <div>
                 <Button bsStyle="primary" bsSize="large" href="/" >
                     GET ME OUT OF HERE!
