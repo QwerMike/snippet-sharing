ed012ffcbcfcd563d820ca5301f2da6c44e060c9
NastyaKolomoets <nastya.kolomoets@gmail.com>
2018-11-25 15:09:56 +0200

    Add navbar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,59 @@
 import React from "react";
 import "./App.css";
 import { Button } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       text: null,
-      isEnabled: false};
+      isEnabled: false
+    };
   }
 
   handleChange() {
-    this.setState({isEnabled: !this.state.isEnabled})
+    this.setState({ isEnabled: !this.state.isEnabled })
   }
 
   render() {
     return (
       <div>
-        <textarea 
-        rows={15}
-        readOnly={this.state.isEnabled ? false : true}
-        >
-          {"html with multiple code snippets"}
-        </textarea>
-        <Button onClick={() => this.handleChange()}>
+        <Navigation/>
+        <div>
+          <textarea
+            rows={15}
+            readOnly={this.state.isEnabled ? false : true}
+          >
+            {"html with multiple code snippets"}
+          </textarea>
+          <Button onClick={() => this.handleChange()}>
           </Button>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+function Navigation(props) {
+  return (
+  <Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#brand">
+          Snippet Share
+        </a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="#">
+          About
+      </NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>)
+}
