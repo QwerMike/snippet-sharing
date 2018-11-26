f14c499deb9161459949f7db0845018fbd6725b9
Mykhailo Kaskun <tmikent@gmail.com>
2018-11-26 02:45:15 +0200

    Add hosting folder

---
+++ cloud-functions/public/precache-manifest.b7323d860f8ddc106c6dec064b6e60ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "245ae8a4780d2fda9688",
+    "url": "/static/js/main.245ae8a4.chunk.js"
+  },
+  {
+    "revision": "dc2d9bb4a6261aa2acb7",
+    "url": "/static/js/1.dc2d9bb4.chunk.js"
+  },
+  {
+    "revision": "245ae8a4780d2fda9688",
+    "url": "/static/css/main.8cb61aaf.chunk.css"
+  },
+  {
+    "revision": "3da3699a43854817506ca23ae373ee63",
+    "url": "/index.html"
+  }
+];
